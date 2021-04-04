4f75dd68e39ee02e8c97c2ad23964d323caf63cb
eduardo3g <eduardosbrasil10@gmail.com>
2021-04-04 12:07:17 -0300

    refactor: move the Sidebar Stack into a separate component

---
+++ src/components/Sidebar/SidebarNav.tsx
@@ -0,0 +1,26 @@
+import { Stack } from '@chakra-ui/react';
+import {
+  RiDashboardLine,
+  RiContactsLine,
+  RiInputMethodLine,
+  RiGitMergeLine
+} from 'react-icons/ri';
+
+import { NavLink } from './NavLink';
+import { NavSection } from './NavSection';
+
+export function SidebarNav() {
+  return (
+    <Stack spacing="12" align="flex-start">
+      <NavSection title="GERAL">
+        <NavLink icon={RiDashboardLine}>Dashboard</NavLink>
+        <NavLink icon={RiContactsLine}>Users</NavLink>
+      </NavSection>
+
+      <NavSection title="AUTOMATION">
+        <NavLink icon={RiInputMethodLine}>Forms</NavLink>
+        <NavLink icon={RiGitMergeLine}>Automation</NavLink>
+      </NavSection>
+    </Stack>
+  );
+};
--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,28 +1,11 @@
-import { Box, Stack } from '@chakra-ui/react';
-import {
-  RiContactsLine,
-  RiDashboardLine,
-  RiGitMergeLine,
-  RiInputMethodLine
-} from 'react-icons/ri';
+import { Box } from '@chakra-ui/react';
 
-import { NavLink } from './NavLink';
-import { NavSection } from './NavSection';
+import { SidebarNav } from './SidebarNav';
 
 export function Sidebar() {
   return (
     <Box as="aside" width="64" marginRight="8">
-      <Stack spacing="12" align="flex-start">
-        <NavSection title="GERAL">
-          <NavLink icon={RiDashboardLine}>Dashboard</NavLink>
-          <NavLink icon={RiContactsLine}>Users</NavLink>
-        </NavSection>
-
-        <NavSection title="AUTOMATION">
-          <NavLink icon={RiInputMethodLine}>Forms</NavLink>
-          <NavLink icon={RiGitMergeLine}>Automation</NavLink>
-        </NavSection>
-      </Stack>
+      <SidebarNav />
     </Box>
   );
 };
