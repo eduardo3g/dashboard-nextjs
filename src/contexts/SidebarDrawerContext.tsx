eecb73bcd40eabf18ffedaf61b035d831b3866ac
eduardo3g <eduardosbrasil10@gmail.com>
2021-04-04 12:20:31 -0300

    feat: store sidebar data in a context

---
+++ src/contexts/SidebarDrawerContext.tsx
@@ -0,0 +1,22 @@
+import { useDisclosure, UseDisclosureReturn } from '@chakra-ui/react';
+import { createContext, ReactNode, useContext } from 'react';
+
+interface SidebarDrawerProviderProps {
+  children: ReactNode;
+}
+
+type SidebarDrawerContextData = UseDisclosureReturn;
+
+const SidebarDrawerContext = createContext({} as SidebarDrawerContextData);
+
+export function SidebarDrawerProvider({ children }: SidebarDrawerProviderProps) {
+  const disclosure = useDisclosure();
+
+  return (
+    <SidebarDrawerContext.Provider value={disclosure}>
+      {children}
+    </SidebarDrawerContext.Provider>
+  );
+};
+
+export const useSidebarDrawer = () => useContext(SidebarDrawerContext);
