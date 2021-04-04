2f93f8244ce960968b7fc9428d9306f6946557d1
eduardo3g <eduardosbrasil10@gmail.com>
2021-04-04 12:02:18 -0300

    style: make the Header responsive

--- src/components/Header/Logo.tsx
+++ src/components/Header/Logo.tsx
@@ -3,19 +3,19 @@ import { Text } from '@chakra-ui/react';
 export function Logo() {
   return (
     <Text
-        fontSize="3xl"
-        fontWeight="bold"
-        letterSpacing="tight"
-        width="64" // 256px or 16rem
+      fontSize={["2xl", "3xl"]}
+      fontWeight="bold"
+      letterSpacing="tight"
+      width="64" // 256px or 16rem
+    >
+      dashgo
+      <Text
+        as="span"
+        marginLeft="1" // 4px or 0.25rem
+        color="pink.500"
       >
-        dashgo
-        <Text
-          as="span"
-          marginLeft="1" // 4px or 0.25rem
-          color="pink.500"
-        >
-          .
-        </Text>
+        .
       </Text>
+    </Text>
   );
 };
--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,12 +1,18 @@
 import { Flex, Box, Text, Avatar } from '@chakra-ui/react';
 
-export function Profile() {
+interface ProfileProps {
+  showProfileData?: boolean;
+};
+
+export function Profile({ showProfileData = true }: ProfileProps) {
   return (
     <Flex align="center">
-      <Box marginRight="4" textAlign="right">
-        <Text>Eduardo Santana</Text>
-        <Text color="gray.300" fontSize="small">eduardosbrasil10@gmail.com</Text>
-      </Box>
+      { showProfileData && (
+        <Box marginRight="4" textAlign="right">
+          <Text>Eduardo Santana</Text>
+          <Text color="gray.300" fontSize="small">eduardosbrasil10@gmail.com</Text>
+        </Box>
+      ) }
 
       <Avatar size="md" name="Eduardo Santana" src="https://github.com/eduardo3g.png" />
     </Flex>
