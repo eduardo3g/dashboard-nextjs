a870ef6be652faf7359b45ac98b46f2e99b90803
eduardo3g <eduardosbrasil10@gmail.com>
2021-04-04 12:13:18 -0300

    style: use Chakra UI drawer to make the sidebar responsive

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,8 +1,44 @@
-import { Box } from '@chakra-ui/react';
+import {
+  Box,
+  Drawer,
+  useBreakpointValue,
+  DrawerContent,
+  DrawerOverlay,
+  DrawerCloseButton,
+  DrawerHeader,
+  DrawerBody
+} from '@chakra-ui/react';
 
 import { SidebarNav } from './SidebarNav';
 
 export function Sidebar() {
+  const isDrawerSideBar = useBreakpointValue({
+    base: true,
+    large: false,
+  });
+
+  // Mobile version
+  if (isDrawerSideBar) {
+    return (
+      <Drawer isOpen={true} placement="left" onClose={() => {}}>
+        <DrawerOverlay>
+          <DrawerContent
+            bg="gray.800"
+            padding="4"
+          >
+            <DrawerCloseButton mt="6" />
+            <DrawerHeader>Navigation</DrawerHeader>
+
+            <DrawerBody>
+              <SidebarNav />
+            </DrawerBody>
+          </DrawerContent>
+        </DrawerOverlay>
+      </Drawer>
+    );
+  }
+
+  // Desktop version
   return (
     <Box as="aside" width="64" marginRight="8">
       <SidebarNav />
