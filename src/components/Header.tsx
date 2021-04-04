b316d0afdf15231856b051668862ca8d2b3ba750
eduardo3g <eduardosbrasil10@gmail.com>
2021-04-04 00:53:22 -0300

    style: add the user name, email and avatar to the header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,17 @@
-import {  Flex, Text, Input, Icon } from '@chakra-ui/react';
-import { RiSearchLine } from 'react-icons/ri';
+import {
+  Flex,
+  Text,
+  Input,
+  Icon,
+  HStack,
+  Box,
+  Avatar
+} from '@chakra-ui/react';
+import {
+  RiNotificationLine,
+  RiSearchLine,
+  RiUserAddLine
+} from 'react-icons/ri';
 
 export function Header() {
   return (
@@ -52,6 +64,33 @@ export function Header() {
         />
 
         <Icon as={RiSearchLine} fontSize="20" />
+      </Flex>
+
+      <Flex
+        align="center"
+        marginLeft="auto"
+      >
+        <HStack
+          spacing="8"
+          marginX="8"
+          paddingRight="8"
+          paddingY="1"
+          color="gray.300"
+          borderRightWidth={1} // pixel
+          borderColor="gray.700"
+        >
+          <Icon as={RiNotificationLine} fontSize="20" />
+          <Icon as={RiUserAddLine} fontSize="20" />
+        </HStack>
+
+        <Flex align="center">
+          <Box marginRight="4" textAlign="right">
+            <Text>Eduardo Santana</Text>
+            <Text color="gray.300" fontSize="small">eduardosbrasil10@gmail.com</Text>
+          </Box>
+
+          <Avatar size="md" name="Eduardo Santana" src="https://github.com/eduardo3g.png" />
+        </Flex>
       </Flex>
     </Flex>
   );
