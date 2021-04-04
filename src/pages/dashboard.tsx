8103ae75917d08593d7507e9b6600e0409ba4402
eduardo3g <eduardosbrasil10@gmail.com>
2021-04-04 01:03:33 -0300

    style: create the sidebar component

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,7 +1,21 @@
+import { Flex } from '@chakra-ui/react';
 import { Header } from '../components/Header';
+import { Sidebar } from '../components/Sidebar';
 
 export default function Dashboard() {
   return (
-    <Header />
+    <Flex direction="column" height="100vh">
+      <Header />
+
+      <Flex
+        width="100%"
+        marginY="6"
+        maxWidth={1480}
+        marginX="auto"
+        paddingX="6"
+      >
+        <Sidebar />
+      </Flex>
+    </Flex>
   );
 };
