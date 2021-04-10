a26f13d5f536893d808f151bb53f6cc73d7bbe23
eduardo3g <eduardosbrasil10@gmail.com>
2021-04-10 12:19:19 -0300

    refactor(services): move the QueryClient instance from the _app to a separate service

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,19 +1,18 @@
 import { AppProps } from 'next/app';
 import { ChakraProvider } from '@chakra-ui/react';
 import { ReactQueryDevtools } from 'react-query/devtools';
-import { QueryClient, QueryClientProvider } from 'react-query';
+import { QueryClientProvider } from 'react-query';
 
 import { theme } from '../styles/theme';
 
 import { SidebarDrawerProvider } from '../contexts/SidebarDrawerContext';
 
 import { makeServer } from '../services/mirage';
+import { queryClient } from '../services/queryClient';
 
 if (process.env.NODE_ENV === 'development') {
   makeServer();
 }
-
-const queryClient = new QueryClient();
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
