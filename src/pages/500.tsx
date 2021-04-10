55a90b078ad5aea6b9f596e70ee0419f9df4ed87
eduardo3g <eduardosbrasil10@gmail.com>
2021-04-10 19:00:02 -0300

    feat: create custom 500 (Server-side error) page

---
+++ src/pages/500.tsx
@@ -0,0 +1,58 @@
+import { Flex, Text, Link } from '@chakra-ui/react';
+import { useRouter } from 'next/router';
+
+export default function Custom404() {
+  const router = useRouter();
+
+  return (
+    <>
+      <Flex
+        width="100vw"
+        height="100vh"
+        alignItems="center"
+        justifyContent="center"
+        flexDirection="column"
+      >
+        <Text
+          fontSize={["2xl", "6xl"]}
+          fontWeight="bold"
+          letterSpacing="tight"
+          marginBottom={4}
+          _hover={{
+            cursor: "pointer"
+          }}
+          color="pink.500"
+        >
+          500
+        </Text>
+
+        <Text>
+          An internal server error ocurred, please contact the support.
+        </Text>
+
+        <Flex
+          flexDirection="row"
+          alignItems="center"
+          justifyContent="cebnter"
+        >
+          <Text
+            fontSize="md"
+            fontWeight="bold"
+            letterSpacing="tight"
+            marginTop={4}
+            color="pink.500"
+          >
+            <Link
+              onClick={() => router.back()}
+              _hover={{
+                textDecoration: "none"
+              }}
+            >
+              Go back.
+            </Link>
+          </Text>
+        </Flex>
+      </Flex>
+    </>
+  );
+};
